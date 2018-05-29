4b11e09470bd820490e6b7ca1721998fa189927e
jw88 <jw88@live.com>
2018-05-29 08:22:38 -0700

    first commit

---
+++ src/main/java/com/example/demo/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
+
+
+//https://cloud.spring.io/spring-cloud-config/
+//https://spring.io/guides/gs/centralized-configuration/
+
+
